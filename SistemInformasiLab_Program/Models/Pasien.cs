d271b7732ee659d46600c3264bb3c86302541a94
Pieter Immanuel Sinaga <pieterimmanuel04@gmail.com>
2025-05-22 21:41:29 +0700

    Menambahkan sistem pendaftaran dan antrian

---
+++ SistemInformasiLab_Program/Models/Pasien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemInformasiLab_Program.Models
+{
+    public class Pasien
+    {
+        public int NomorAntrian { get; set; }
+        public string Nama { get; set; }
+        public string NIK { get; set; }
+        public string JenisKelamin { get; set; }
+        public string JenisPasien { get; set; }
+        public string Keluhan { get; set; }
+        public DateTime WaktuDaftar { get; set; }
+    }
+
+}
--- SistemInformasiLab_Program/Program.cs
+++ SistemInformasiLab_Program/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using SistemInformasiLab_Program.Services;
+using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("Data/KuotaPasien.json")
+            .Build();
+
+
+        int batasAntrian = int.Parse(config["Kuota"]);
+        var sistem = new SistemPendaftaran(batasAntrian);
+
+        while (true)
+        {
+            Console.WriteLine("=== SISTEM PENDAFTARAN LABORATORIUM RSUD KAJEN ===");
+            Console.WriteLine("1. Pendaftaran");
+            Console.WriteLine("2. Antrian");
+            Console.WriteLine("3. Keluar");
+            Console.Write("Pilih menu: ");
+            string pilihan = Console.ReadLine();
+
+            switch (pilihan)
+            {
+                case "1":
+                    sistem.DaftarPasien();
+                    break;
+                case "2":
+                    sistem.LihatAntrian();
+                    break;
+                case "3":
+                    Console.WriteLine("Terima kasih!");
+                    return;
+                default:
+                    Console.WriteLine("Pilihan tidak tersedia.\n");
+                    break;
+            }
+        }
+    }
+}
