abe4844ed2eb8328101efce9c3a8ace756d01423
alfianrizky <alfianrizkysabiann@gmail.com>
2025-05-24 06:43:08 +0700

    menambahkan unit test (belum selesai)

---
+++ LabSystem.Tests/PasienServiceTests.cs
@@ -0,0 +1,38 @@
+﻿public class PasienServiceTests
+{
+    private PasienService BuatPasienServiceUntukTest()
+    {
+        // Pastikan file konfigurasi_test.json tersedia di direktori output (bin)
+        var path = Path.Combine(AppContext.BaseDirectory, "konfigurasi_test.json");
+        var konfigurasiService = new KonfigurasiService(path);
+        return new PasienService(konfigurasiService);
+    }
+
+    [Fact]
+    public void CariPasien_IdValid_ReturnsPasien()
+    {
+        var service = BuatPasienServiceUntukTest();
+        var pasien = service.CariPasien("0101010101010101");
+
+        Assert.NotNull(pasien);
+        Assert.Equal("Andi", pasien?.Nama);
+    }
+
+    [Fact]
+    public void CariPasien_IdTidakDitemukan_ReturnsNull()
+    {
+        var service = BuatPasienServiceUntukTest();
+        var pasien = service.CariPasien("9999999999999999");
+
+        Assert.Null(pasien);
+    }
+
+    [Fact]
+    public void CariPasien_IdKosong_ThrowArgumentException()
+    {
+        var service = BuatPasienServiceUntukTest();
+
+        var ex = Assert.Throws<ArgumentException>(() => service.CariPasien(""));
+        Assert.Equal("ID tidak boleh kosong", ex.Message);
+    }
+}
---
+++ LabSystem.Tests/PemeriksaanTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using SistemInformasiLab_Program.Models;
+
+public class PemeriksaanTests
+{
+    [Fact]
+    public void Keterangan_HasilLebihRendah_ReturnsL()
+    {
+        var p = new Pemeriksaan { Hasil = 10, NilaiNormal = (11.7, 15.5) };
+        Assert.Equal("L", p.Keterangan);
+    }
+
+    [Fact]
+    public void Keterangan_HasilLebihTinggi_ReturnsH()
+    {
+        var p = new Pemeriksaan { Hasil = 16, NilaiNormal = (11.7, 15.5) };
+        Assert.Equal("H", p.Keterangan);
+    }
+
+    [Fact]
+    public void Keterangan_HasilNormal_ReturnsEmptyString()
+    {
+        var p = new Pemeriksaan { Hasil = 13, NilaiNormal = (11.7, 15.5) };
+        Assert.Equal("", p.Keterangan);
+    }
+
+    [Fact]
+    public void Keterangan_HasilNull_ReturnsStrip()
+    {
+        var p = new Pemeriksaan { Hasil = null, NilaiNormal = (11.7, 15.5) };
+        Assert.Equal("-", p.Keterangan);
+    }
+}
