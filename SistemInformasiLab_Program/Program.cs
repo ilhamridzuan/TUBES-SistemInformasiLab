6f5b61be8120099cd6dbf6aeecb7fc98dada7fdd
ilhamridzuan <ilhamridzuaan@gmail.com>
2025-05-23 14:45:56 +0700

    Menambahkan implementasi fitur login & register akun

--- SistemInformasiLab_Program/Program.cs
+++ SistemInformasiLab_Program/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SistemInformasiLab_Program.Controllers;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        AuthController controller = new AuthController();
+        await controller.ShowMenuAsync(); // Menampilkan menu login/registrasi
+    }
+}
---
+++ SistemInformasiLab_Program/Services/HttpService.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System;
+using SistemInformasiLab_Program.Models;
+
+namespace SistemInformasiLab_Program.Services
+{
+    public class HttpService
+    {
+        private readonly HttpClient _client;
+
+        public HttpService(string baseAddress)
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+        }
+
+        public async Task<RegisterResponse> RegisterAsync(string fullname, string email, string username, string password, string role)
+        {
+            var response = await _client.PostAsJsonAsync("api/auth/register", new
+            {
+                Fullname = fullname,
+                Email = email,
+                Username = username,
+                Password = password,
+                Role = role
+            });
+
+            return await response.Content.ReadFromJsonAsync<RegisterResponse>();
+        }
+
+        public async Task<LoginResponse> LoginAsync(string username, string password)
+        {
+            var response = await _client.PostAsJsonAsync("api/auth/login", new
+            {
+                Username = username,
+                Password = password
+            });
+
+            return await response.Content.ReadFromJsonAsync<LoginResponse>();
+        }
+
+    }
+}
